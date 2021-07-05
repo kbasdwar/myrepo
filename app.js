0fcabcccfec2e8d3bce9946d9876d78e399f7a23
k47billion <khushboo.basdwar@47billion.com>
2021-07-05 13:30:39 +0530

    Initial commit.

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const dotenv  = require('dotenv')
+const morgan = require('morgan')
+const exphbs=require('express-handlebars')
+const connectDB =require('./config/db')
+
+//load config
+dotenv.config({path: './config/config.env'})
+
+connectDB()
+
+const app =express()
+
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+
+//Handlebars
+app.engine('.hbs',exphbs({defaultLayout:'main', extname: '.hbs'}));
+app.set('view engine','.hbs');
+
+//Routes
+app.use('/',require('./routes/index'))
+
+
+const PORT =process.env.PORT || 6000
+
+app.listen(
+    PORT,
+    console.log(`Server running in ${process.env.NODE_ENV} mode on Port ${PORT}` )
+    )
